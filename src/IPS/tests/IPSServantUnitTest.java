a67601dba0edcd4997f1192f6126f711adac9174
amendoza <daniel.mendo04@gmail.com>
2020-11-20 16:10:59 -0500

    IPSServer UnitTest passed

--- src/IPS/tests/IPSServantUnitTest.java
+++ src/IPS/tests/IPSServantUnitTest.java
@@ -1,5 +1,28 @@
 package IPS.tests;
 
+import IPS.controller.IIPS;
+import IPS.controller.IPSServant;
+import IPS.persistence.IPSPersistence;
+import org.junit.Test;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class IPSServantUnitTest {
 
+    @Test
+    public void pedirVacunasSatisfactoriamente() throws RemoteException {
+        IIPS ips = new IPSServant(new IPSPersistence("src/IPS/tests/testConfig.txt","src/IPS/tests/vacunasIps.txt"));
+
+        List<Integer> res = ips.pedirVacunas(5,5,5);
+        List<Integer> correctAnswer = new ArrayList<>();
+        correctAnswer.add(10);
+        correctAnswer.add(10);
+        correctAnswer.add(10);
+        assertEquals(res, correctAnswer);
+
+    }
 }
