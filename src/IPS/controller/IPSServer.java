3e5946e7b3a2ebe488ad6e1483c89956e6058721
david <sardavid13@hotmail.com>
2020-11-23 23:22:21 -0500

    Two IPS

--- src/IPS/controller/IPSServer.java
+++ src/IPS/controller/IPSServer.java
@@ -1,6 +1,5 @@
 package IPS.controller;
 
-import IPS.controller.IPSServant;
 import IPS.model.IPSData;
 import IPS.persistence.IPSPersistence;
 
@@ -14,10 +13,11 @@ public class IPSServer {
 
         //int port = 8888;
         //String nombre = "IPS";
-        IPSPersistence persistence = new IPSPersistence("src/IPS/tests/testConfig.txt","src/IPS/tests/vacunasIps.txt", "src/IPS/tests/ipsData.txt");
+        IPSPersistence persistence = new IPSPersistence("src/IPS/tests/vacunasIps2.txt","src/IPS/tests/vacunasIps2.txt", "src/IPS/tests/ipsData2.txt");
 
         IPSData ips = persistence.readIPSFile();
 
+        System.setProperty("java.rmi.server.hostname","25.0.180.133");
         Registry registry = LocateRegistry.createRegistry(ips.getPort());
         registry.rebind(ips.getName(), new IPSServant(persistence));
 
